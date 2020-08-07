c71b81d8ec7e394ae2034fc2337209d2f61628aa
gudals113 <61488937+gudals113@users.noreply.github.com>
2020-08-07 14:41:15 +0900

    todoApp dlgudals v1

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 const bookRouter = require('./routes/index')
 const cors = require('cors')
 const app = express();
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 4700;
 
 // Node.js의 native Promise 사용
 mongoose.Promise = global.Promise;
